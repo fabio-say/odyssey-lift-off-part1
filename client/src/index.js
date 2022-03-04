98dcb941857ff8036a24cfe455a31c67b668ae42
Fabio Say <fabio.say@tuntscorp.com>
2022-03-04 18:57:30 -0300

    A

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,26 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import {Layout} from "./components";
+import TrackCard from "./containers/track-card";
+
+
+const client = new ApolloClient({
+    // options go here
+    uri: 'http://localhost:4000',
+    cache: new InMemoryCache(),
+
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+        <React.StrictMode>
+            <GlobalStyles/>
+            <Pages/>
+        </React.StrictMode>
+    </ApolloProvider>,
+    document.getElementById('root')
 );
--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,24 @@
-//TODO
+const { ApolloServer, RedisCache } = require('apollo-server');
+const resolvers = require('./resolvers');
+const TrackAPI = require('./datasouces/track-api');
+
+
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+        return {
+            trackAPI: new TrackAPI()
+        };
+    }
+});
+
+server.listen().then(() => {
+    console.log(`
+    🚀  Server is running!
+    🔉  Listening on port 4000
+    📭  Query at https://studio.apollographql.com/dev
+  `);
+});
